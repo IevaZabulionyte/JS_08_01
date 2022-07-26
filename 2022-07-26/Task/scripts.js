225db7f879f1b3d75b0747576adf7d7431f25bf9
IevaZabulionyte <ieva.zabulionyte12@gmail.com>
2022-07-26 21:26:27 +0300

    2022-07-26

---
+++ 2022-07-26/Task/scripts.js
@@ -0,0 +1,26 @@
+//prompt("enter the value No. 1:")
+
+let input1 = prompt ("enter the value No. 1:");
+
+let input2 = prompt ("enter the value No. 2:");
+
+sum = Number.parseInt(input1) + Number.parseInt(input2);
+alert("The result of addition = " + sum);
+
+substraction = Number.parseInt(input1) - Number.parseInt(input2);
+alert("The result of substraction = " + substraction);
+
+multiplication = Number.parseInt(input1) * Number.parseInt(input2);
+alert("The result of multiplication = " + multiplication);
+
+division = Number.parseInt(input1) / Number.parseInt(input2);
+alert("The result of division = " + division);
+
+
+
+let name = prompt ("Please, write your name");
+let surname = prompt ("Please, write your surname");
+
+both = name + surname;
+
+alert("Hello " + name + " " + surname);
---
+++ 2022-07-26/script.js
@@ -0,0 +1,72 @@
+
+alert("hello");
+
+//console.log("text");
+
+
+//let a = 2;
+//let b = 4;
+//console.log(a);
+//a = 102;
+//console.log(b);
+
+/*
+comment
+block
+comment
+*/
+
+//"use strict";
+//variable1 = 1
+var var1 = 123;
+console.log(var1); {
+var var1 = 189;
+//let var3 = 90; - taip daryt negalima, nes liko tik bloko viduje
+console.log(var1);
+
+}
+console.log(var1);
+
+typeof(var1);
+
+let a = 5;
+let b = 8;
+let result = a + b;
+console.log(result);
+
+let name = "Arturs";
+let lastname = "Olekss";
+let result1 = name + lastname;
+console.log(result1);
+
+let element = "10";
+let element1 = "101";
+result2 = element - element1;
+console.log(result2);
+result = name - element;
+console.log(result);
+
+
+result2 = Number.parseInt(element) + Number.parseInt(element1);
+console.log(result2);
+
+
+element = "10.45"
+element1 = "101.23"
+result2 = Number.parseInt(element) + Number.parseInt(element1);
+console.log(result2);
+
+
+element = "10.45"
+element1 = "101.23"
+result2 = Number.parseFloat(element) + Number.parseFloat(element1);
+console.log(result2);
+
+element = 123456;
+element1 = 2;
+result = element / element1;
+console.log("the result is " + result);
+
+
+result = element % element1;
+console.log("the result is " + result);
